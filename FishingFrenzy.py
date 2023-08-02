8769b9be0a9e7cb40e6ce3f4dc9defa7b8808ea6
Eshwar Muneshar <Emuneshar@gmail.com>
2023-08-02 17:50:29 -0400

    Started coding fishing frenzy game

---
+++ FishingFrenzy.py
@@ -0,0 +1,14 @@
+import random, time
+
+fish = {
+  "sea bass": 1,
+  "blobfish": 2,
+  "trout": 3,
+  "hammerhead Shark": 100,
+  "goldfish": 2, 
+  "lion fish": 40,
+  "catfish": 10,
+  "megalodon": 4000000000,
+  "salmon": 5
+}
+
